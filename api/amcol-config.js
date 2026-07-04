d23a4e002ae6626756290592d247b41901757500
Justinpoulido <jpoulido@gmail.com>
2026-07-04 15:13:18 -0400

    Add admin portal and homepage updates

--- api/amcol-config.js
+++ api/amcol-config.js
@@ -1,15 +1,23 @@
-module.exports = function handler(_req, res) {
-    const config = {
-        mapboxToken: process.env.MAPBOX_PUBLIC_TOKEN || "",
-        supabaseUrl: process.env.SUPABASE_URL || "",
-        supabasePublishableKey: process.env.SUPABASE_PUBLISHABLE_KEY || ""
-    };
+function getAmcolConfig() {
+    const env = typeof process !== "undefined" && process.env ? process.env : {};
 
-    res.setHeader("Content-Type", "application/javascript; charset=utf-8");
-    res.setHeader("Cache-Control", "no-store, max-age=0");
-    res.status(200).send(
-        "window.AMCOL_CONFIG = Object.assign({}, window.AMCOL_CONFIG || {}, " +
-        JSON.stringify(config) +
-        ");"
-    );
-};
+    return {
+        mapboxToken: env.MAPBOX_PUBLIC_TOKEN || "",
+        supabaseUrl: env.SUPABASE_URL || "",
+        supabasePublishableKey: env.SUPABASE_PUBLISHABLE_KEY || ""
+    };
+}
+
+if (typeof module !== "undefined" && module.exports) {
+    module.exports = function handler(_req, res) {
+        res.setHeader("Content-Type", "application/javascript; charset=utf-8");
+        res.setHeader("Cache-Control", "no-store, max-age=0");
+        res.status(200).send(
+            "window.AMCOL_CONFIG = Object.assign({}, window.AMCOL_CONFIG || {}, " +
+            JSON.stringify(getAmcolConfig()) +
+            ");"
+        );
+    };
+} else if (typeof window !== "undefined") {
+    window.AMCOL_CONFIG = Object.assign({}, window.AMCOL_CONFIG || {}, getAmcolConfig());
+}
